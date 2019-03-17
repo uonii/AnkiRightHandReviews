f6c1081db3547342a2ca8db0fc7b876d180c88c2
Austin Hasten <16011612+AustinHasten@users.noreply.github.com>
2019-03-17 10:39:11 -0500

    Remove silly functionality
    Removed the "select answer closest to what you pressed" functionality, as well as the "answer without revealing the back of the card" functionality. Switched to wrapping rather than inheritance.

--- __init__.py
+++ __init__.py
@@ -1,41 +1,15 @@
 # Austin Hasten
 # Initial Commit - January 13th, 2018
 
-from aqt import mw
+from anki.hooks import wrap
 from aqt.reviewer import Reviewer
 
-class RightHandReviewer(Reviewer):
-    # Keep Python from complaining that self.shortcuts doesn't exist. 
-    shortcuts = []
+def newShortcutKeys(self, _old):
+    return _old(self) + [
+        ("j", lambda: self._answerCard(1)),
+        ("k", lambda: self._answerCard(2)),
+        ("l", lambda: self._answerCard(3)),
+        (";", lambda: self._answerCard(4)),
+    ]
 
-    def __init__(self, mw):
-        super().__init__(mw)
-
-        # Reviewer's default shortcuts, plus our own.
-        self.shortcuts = super()._shortcutKeys() + [
-            ("j", lambda: self._answerCard(1)),
-            ("k", lambda: self._answerCard(2)),
-            ("l", lambda: self._answerCard(3)),
-            (";", lambda: self._answerCard(4)),
-        ]
-
-    def _shortcutKeys(self):
-        """ Return our shortcuts rather than the default ones. """
-        return self.shortcuts
-
-    def _answerCard(self, ease):
-        """
-        Modified copy of aqt.Reviewer._answerCard.
-        Allows cards to be answered without first showing the back.
-        Also selects the nearest difficulty level to the users selection.
-        """
-        if self.mw.state != "review":
-            return
-        ease = min(ease, self.mw.col.sched.answerButtons(self.card))
-        self.mw.col.sched.answerCard(self.card, ease)
-        self._answeredIds.append(self.card.id)
-        self.mw.autosave()
-        self.nextCard()
-
-# Use our modified Reviewer rather than the default one.
-mw.reviewer = RightHandReviewer(mw)
+Reviewer._shortcutKeys = wrap(Reviewer._shortcutKeys, newShortcutKeys, "around")
